5c045d7621b43c9cc4ca60c9d73f779e5f104098
Wingkou Loeng <961574992@qq.com>
2018-06-01 10:30:56 +0800

    Add project files.

---
+++ AutoCore/AdbWrapper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace AutoCore
+{
+    public class AdbWrapper
+    {
+        private string adbPath, adbDir;
+        private Random random = new Random();
+
+        public AdbWrapper()
+        {
+            adbDir = AppDomain.CurrentDomain.BaseDirectory;
+            adbPath = Path.Combine(adbDir, "adb.exe");
+        }
+
+        public string AdbCommand(string arg)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = adbPath;
+                process.StartInfo.WorkingDirectory = adbDir;
+                process.StartInfo.Arguments = arg;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                var result = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                process.Close();
+                return result;
+            }
+        }
+
+        public void Tap(Point x)
+        {
+            var args = $"shell input tap {x.X} {x.Y}";
+            AdbCommand(args);
+        }
+
+        public void Tap2(Point x)
+        {
+            var args = "shell sendevent /dev/input/event2 {0} {1} {2}";
+
+            AdbCommand(string.Format(args, 3, 0, x.X));
+            AdbCommand(string.Format(args, 3, 1, x.Y));
+
+            AdbCommand(string.Format(args, 1, 330, 1));
+            AdbCommand(string.Format(args, 0, 0, 0));
+
+            AdbCommand(string.Format(args, 1, 330, 0));
+            AdbCommand(string.Format(args, 0, 0, 0));
+        }
+
+        public void Swipe(Point x1, Point x2, int time)
+        {
+            var args = $"shell input swipe {x1.X} {x1.Y} {x2.X} {x2.Y} {time}";
+            AdbCommand(args);
+        }
+
+        public void LongPress(Point x, int time) => Swipe(x, x, time);
+
+        public void Text(string text)
+        {
+            var args = $"shell input text \"{text}\"";
+            AdbCommand(args);
+        }
+
+        public Bitmap GetScreenshot()
+        {
+            var fileDirectory = Path.GetTempPath();
+            var fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".png";
+            var createFileArg = $"shell screencap -p /sdcard/{fileName}";
+            AdbCommand(createFileArg);
+            var downloadArg = $"pull /sdcard/{fileName} {fileDirectory}";
+            AdbCommand(downloadArg);
+            var deleteFileArg = $"shell rm /sdcard/{fileName}";
+            AdbCommand(deleteFileArg);
+            var imgFilePath = Path.Combine(fileDirectory, fileName);
+            if (!File.Exists(imgFilePath))
+                return null;
+            return new Bitmap(imgFilePath);
+        }
+
+        public IEnumerable<string> GetDeciveID()
+        {
+            var delim0 = new string[] { Environment.NewLine };
+            var delim1 = new string[] { " ", "\t" };
+            var option = StringSplitOptions.RemoveEmptyEntries;
+
+            var devicesArg = "devices";
+            var result = AdbCommand(devicesArg);
+
+            var lines = result.Split(delim0, option);
+            return lines.Skip(1).Select(l => l.Split(delim1, option).First());
+        }
+    }
+}
---
+++ AutoSudoku/Program.cs
@@ -0,0 +1,81 @@
+﻿using AutoCore;
+using SudokuSolverLib;
+using System;
+using System.Drawing;
+using System.Text;
+using Tesseract;
+
+namespace AutoSudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdbWrapper adb = new AdbWrapper();
+
+            int[][] board = new int[9][];
+            StringBuilder sboard = new StringBuilder();
+            Rectangle boardRegion = new Rectangle(55, 407, 966, 966);
+            float offset = 5;
+
+            using (var img = adb.GetScreenshot())
+            using (var boardImg = img.Clone(boardRegion, img.PixelFormat))
+            {
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.TesseractAndCube))
+                {
+                    engine.SetVariable("tessedit_char_whitelist", "0123456789");
+                    for (int i = 0; i < 9; i++)
+                    {
+                        board[i] = new int[9];
+                        for (int j = 0; j < 9; j++)
+                        {
+                            float x = boardImg.Width / 9f * j + offset;
+                            float y = boardImg.Height / 9f * i + offset;
+                            float w = boardImg.Width / 9f - offset * 2f;
+                            float h = boardImg.Height / 9f - offset * 2f;
+                            using (var cell = boardImg.Clone(new RectangleF(x, y, w, h), boardImg.PixelFormat))
+                            using (var cellx = new Bitmap(cell, new Size(cell.Width / 4, cell.Height / 4)))
+                            using (var page = engine.Process(cellx, PageSegMode.SingleChar))
+                            {
+                                var text = page.GetText();
+                                if (string.IsNullOrWhiteSpace(text))
+                                {
+                                    board[i][j] = 0;
+                                    sboard.Append('.');
+                                }
+                                else
+                                {
+                                    board[i][j] = text[0] - '0';
+                                    sboard.Append(text[0]);
+                                }
+                            }
+                        }
+                        sboard.AppendLine();
+                    }
+                }
+            }
+            
+            var p = SudokuPuzzle.FromString(sboard.ToString(), 3, 3);
+            Console.WriteLine(p);
+            p.SolveGrid();
+            Console.WriteLine(p);
+            p.SolveGrid();
+            
+            int kx = 160 + 75;
+            int ky = 1460 + 75;
+            int cellh = boardRegion.Height / 9;
+            int cellw = boardRegion.Width / 9;
+            int bx = boardRegion.X + boardRegion.Width / 18;
+            int by = boardRegion.Y + boardRegion.Height / 18;
+
+            int index = 0;
+            foreach (var node in p.GetNodes())
+            {
+                int v = node.Value;
+                adb.Tap(new Point(kx + v % 5 * 150, ky + v / 5 * 150));
+                adb.Tap(new Point(bx + index % 9 * cellw, by + index / 9 * cellh));
+                index++;
+            }
+        }
+    }
+}
